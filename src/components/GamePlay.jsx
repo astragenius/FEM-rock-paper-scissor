cd66f6d2f55ec26bad4ae2a6b1ad51d7b011762c
astragenius <darthmalak86@gmail.com>
2024-03-09 18:06:41 +0100

    added gamePlay and playAgain component

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Placeholder from "./Placeholder";
+import PlayAgain from "./PlayAgain";
 import Symbol from "./Symbol";
 
 const GamePlay = () => {
 	const [isPending, setPending] = useState(true);
+	const [modal, showModal] = useState(false);
+
+	useEffect(() => {
+		setInterval(() => {
+			setPending(false);
+		}, 1000);
+	});
+
+	useEffect(() => {
+		setInterval(() => {
+			showModal(true);
+		}, 2000);
+	}, [isPending]);
 	return (
 		<>
 			<div className="gameBoardPlay">
@@ -11,6 +25,7 @@ const GamePlay = () => {
 					<h3 className="fs-24-15 text-uppercase ls-30 fw-600">You Picked</h3>
 					<Symbol key={1} name={"rock"} icon={"/public/Icons/icon-rock.svg"} />
 				</div>
+				{modal && <PlayAgain status={"Draw"} />}
 
 				<div className="container">
 					<h3 className="fs-24-15 text-uppercase ls-30 fw-600">
---
+++ src/components/PlayAgain.jsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+
+const PlayAgain = ({ status }) => {
+	return (
+		<div className="playAgain-container">
+			<h3 className="fs-56 text-uppercase fw-700">{status}</h3>
+			<motion.button
+				whileHover={{ color: "#DB2E4D", scale: 1.1 }}
+				whileTap={{ scale: 0.8 }}
+				className="play-btn text-uppercase ls-25"
+			>
+				Play it Again
+			</motion.button>
+		</div>
+	);
+};
+
+export default PlayAgain;
