2efd8e99dc915d8c4da9aaeae92c57d947216a59
astragenius <darthmalak86@gmail.com>
2024-03-09 14:59:37 +0100

    add placeholder component

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
+import Placeholder from "./Placeholder";
 import Symbol from "./Symbol";
 
 const GamePlay = () => {
+	const [isPending, setPending] = useState(true);
 	return (
 		<>
 			<div className="gameBoardPlay">
-				<div>
+				<div className="container">
 					<h3 className="fs-24-15 text-uppercase ls-30 fw-600">You Picked</h3>
 					<Symbol key={1} name={"rock"} icon={"/public/Icons/icon-rock.svg"} />
 				</div>
 
-				<div>
+				<div className="container">
 					<h3 className="fs-24-15 text-uppercase ls-30 fw-600">
 						The House Picked
 					</h3>
-					<Symbol key={1} name={"rock"} icon={"/public/Icons/icon-rock.svg"} />
+
+					{isPending ? (
+						<Placeholder />
+					) : (
+						<Symbol
+							key={1}
+							name={"rock"}
+							icon={"/public/Icons/icon-rock.svg"}
+						/>
+					)}
 				</div>
 			</div>
 		</>
